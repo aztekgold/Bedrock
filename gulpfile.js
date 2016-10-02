a1b22fff359360f0b99569e530eaa14258d6b0c5
Matt Smith <matt.sm@outlook.com>
2016-10-02 16:08:35 +0100

    updated sassets

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
 var wiredep = require('wiredep').stream;
 
 gulp.task('bower', function () {
@@ -8,4 +12,29 @@ gulp.task('bower', function () {
       // goes: 'here'
     }))
     .pipe(gulp.dest('./dist'));
+});
+
+// Compile Sass and Autoprefix
+gulp.task('sass', function() {
+    return gulp.src('./app/styles/main.scss')
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(autoprefixer({
+        browsers: '> 5%'
+    }))
+    .pipe(gulp.dest('./public/styles/'))
+})
+
+gulp.task('scripts', function(){
+    return gulp.src('./app/scripts/*.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/scripts/'))
+})
+
+gulp.task('imagemin', function(){
+    gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'))
 });
